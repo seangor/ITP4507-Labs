1a80bd8dd5724ca53fd308e711d14ccd369d3fcc
seangor <145199909+seangor@users.noreply.github.com>
2023-09-16 12:56:00 +0000

    Submit Lab01

--- Lab01/Lab01Q4.java
+++ Lab01/Lab01Q4.java
@@ -1,7 +1,7 @@
 public class Lab01Q4 {
 
 	public static void main(String[] args) {
-		Cal cal = new Cal();
-		cal.compute();
+		Cal2 cal2 = new Cal2();
+		cal2.compute();
 	}
 }
--- Lab01/TestWriteText.java
+++ Lab01/TestWriteText.java
@@ -1,20 +1,20 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class TestWriteText {
-	public static void main(String[] args){
-		try {
-			FileWriter outputFile = new FileWriter(args[0]);
-			PrintWriter out = new PrintWriter(outputFile);
-			
-			// Write text to file
-			out.println("line 1");
-			out.println("line 2");
-			// write the text from buffer to the file
-			out.flush();
-			// close the file after use
-			out.close();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
+public static void main(String[] args) {
+
+    String data = "This is a text inside the file.";
+
+    try {
+      PrintWriter output = new PrintWriter("output.txt");
+
+      output.print(data);
+      output.close();
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
 	}
-}
